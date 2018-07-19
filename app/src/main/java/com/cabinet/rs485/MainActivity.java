1f38b00f4916cb431973a6eedb50b72f241a9486
tianfeng <286595632@qq.com>
2018-07-19 09:01:04 +0800

    init Ch34xDriver

--- app/src/main/java/com/cabinet/rs485/MainActivity.java
+++ app/src/main/java/com/cabinet/rs485/MainActivity.java
@@ -2,6 +2,7 @@ package com.cabinet.rs485;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cabinet.rs485.rs485.ControlRequest;
 import com.cabinet.rs485.rs485.Item.data.DataLineResponse;
@@ -15,7 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ControlRequest.getInstance().dataLineRequest(new Response.Listener<DataLineResponse>() {
+        try {
+            ControlRequest.getInstance().initCH34x(this);
+        } catch (RS485Error rs485Error) {
+            rs485Error.printStackTrace();
+            Log.e("TAG", "rs485Error: "+rs485Error.getMessage());
+        }
+
+        ControlRequest.getInstance()
+                .dataLineRequest(new Response.Listener<DataLineResponse>() {
             @Override
             public void onResponse(DataLineResponse response) {
                 // on response callback
